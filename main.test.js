abff0853afc9bcd010b345b074891ba99336d17d
Jeff <jeffersonasto95@gmail.com>
2023-07-27 20:43:52 +0100

    Adding FizzBuzzFuntion

---
+++ main.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+
+describe("Given the function FizzBuzzFunction ", () => {
+  describe("when the function is called with (0,15)", () => {
+    test("Then array should be []", () => {
+      const start = 0;
+      const end = 15;
+      const expected = [
+        "fizzbuzz",
+        1,
+        2,
+        "fizz",
+        4,
+        "buzz",
+        "fizz",
+        7,
+        8,
+        "fizz",
+        "buzz",
+        11,
+        "fizz",
+        13,
+        14,
+        "fizzbuzz",
+      ];
+    });
+  });
+});
